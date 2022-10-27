452837dca9f6781db45ad39861e75bb44c597863
root <wdg369@gmail.com>
2022-10-27 13:24:15 +0000

    ADD main part to preprocessing.py

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -3,7 +3,7 @@ from emoji import core
 # from quickspacer import Spacer
 from hanspell import spell_checker
 from soynlp.normalizer import emoticon_normalize
-
+import pandas as pd
 
 class Preprocessing():
 
@@ -26,14 +26,33 @@ class Preprocessing():
             # 에러 발생시 맞춤법 교정은 생략하고 다음 단계로 넘김.
             spell_checked = sentence
 
-        # emoticon_normalize가 'ㅋㅋㅋ' 연쇄 뒤의 음절을 지우지 않도록 해당 연쇄 뒤에 공백 추가
-        p = re.compile('[ㄱ-ㅎ]{1,}')
+        # emoticon_normalize가 'ㅋㅋㅋ' 연쇄 앞뒤의 음절을 지우지 않도록 해당 연쇄 앞뒤에 공백 추가
+        p = re.compile('[ㄱ-ㅎ]{2,}')
         pattern_list = p.findall(spell_checked)
         if pattern_list:
             for pattern in pattern_list:
-                spell_checked = spell_checked.replace(pattern, pattern + ' ')
+                spell_checked = spell_checked.replace(pattern, ' ' + pattern + ' ')
         
         # '맞앜ㅋㅋ' -> '맞아 ㅋㅋㅋ' 와 같이 정규화합니다.   
         normalized = emoticon_normalize(spell_checked, num_repeats=2)
 
         return normalized.strip()
+
+# 학습 전에 전처리를 미리 하고 싶다면 python preprocessing.py를 실행한다.
+if __name__ == '__main__':
+    prepro = Preprocessing()
+    train_df = pd.read_csv('../data/train.csv')
+    dev_df = pd.read_csv('../data/dev.csv')
+    test_df = pd.read_csv('../data/test.csv')
+
+    train_df['sentence_1'] = train_df['sentence_1'].apply(lambda x: prepro.preprocessing(x))
+    train_df['sentence_2'] = train_df['sentence_2'].apply(lambda x: prepro.preprocessing(x))
+    train_df.to_csv('../data/train_preprocessed.csv')
+
+    dev_df['sentence_1'] = dev_df['sentence_1'].apply(lambda x: prepro.preprocessing(x))
+    dev_df['sentence_2'] = dev_df['sentence_2'].apply(lambda x: prepro.preprocessing(x))
+    dev_df.to_csv('../data/dev_preprocessed.csv')
+
+    # test_df['sentence_1'] = test_df['sentence_1'].apply(lambda x: prepro.preprocessing(x))
+    # test_df['sentence_2'] = test_df['sentence_2'].apply(lambda x: prepro.preprocessing(x))
+    # test.to_csv('../data/test_preprocessed.csv')
