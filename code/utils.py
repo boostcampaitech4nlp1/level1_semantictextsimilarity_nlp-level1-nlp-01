88c64d159ba6c19d1effa7d29bb83cdd7cd0d2e1
54data <ehdvkf02@naver.com>
2022-11-01 09:54:36 +0000

    ADD optimizer options in utils.py

--- code/utils.py
+++ code/utils.py
@@ -10,11 +10,13 @@ def optimizer_selector(name, params, lr):
     if name == "Adam":
         optimizer = torch.optim.Adam(params=params, lr=lr)
     elif name == 'AdamW':
-        optimizer = torch.optim.AdamW(params=params,lr=lr)
-    elif name == 'SGD':
-        optimizer = torch.optim.SGD(params=params,lr=lr)
+        optimizer = torch.optim.AdamW(params=params, lr=lr)
+    elif name == 'RMSprop':
+        optimizer = torch.optim.RMSprop(params=params, lr=lr)
+    elif name == 'NAdam':
+        optimizer = torch.optim.NAdam(params=params, lr=lr)
     elif name == 'RAdam':
-        optimizer = torch.optim.RAdam(params=params,lr=lr)
+        optimizer = torch.optim.RAdam(params=params, lr=lr)
 
     return optimizer
 
